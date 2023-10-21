c8a000845fdad499cecc5ba4ae54d4f9f4429656
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-21 16:31:18 -0700

    Update State - Type - Object

---
+++ react/StateUpdateObjects.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const StateUpdateObjects = () => {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Fix Bug 1", fixed: false },
+    { id: 2, title: "Fix Bug 2", fixed: false },
+  ]);
+
+  const handleBugFix = (e: any) => {
+    const bugId = e.currentTarget.getAttribute("data-value");
+    const updatedState = bugs.map((bug) => {
+      return bug.id == bugId ? { ...bug, title: "Fixed", fixed: true } : bug;
+    });
+
+    setBugs(updatedState);
+  };
+
+  const listOfBugs = bugs.map((bug, idx) => {
+    return (
+      <button key={bug.id} data-value={bug.id} onClick={handleBugFix}>
+        {bug.title}
+      </button>
+    );
+  });
+
+  return <div>{listOfBugs}</div>;
+};
