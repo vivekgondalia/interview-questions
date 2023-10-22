c0864910816b1cb2effad93374983220cac97166
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-21 17:04:57 -0700

    Adding comment to explain two concepts of State Update

--- react/StateUpdateObjects.tsx
+++ react/StateUpdateObjects.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
 export const StateUpdateObjects = () => {
+  //This is an example of two concepts for Updating State
+  // 1) Updating Object and 2) Updating Array of Objects
+
   const [bugs, setBugs] = useState([
     { id: 1, title: "Fix Bug 1", fixed: false },
     { id: 2, title: "Fix Bug 2", fixed: false },
