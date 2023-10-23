367109cfc68146f2bd13c415eda77e57015b7b11
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-22 21:55:52 -0700

    Http Get Async Await

---
+++ react/HttpGetAsyncAwait.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function HttpGetAsyncAwait() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+      fetchUsers();
+    };
+  }, []);
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default HttpGetAsyncAwait;
