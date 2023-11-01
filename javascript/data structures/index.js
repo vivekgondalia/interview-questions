a3c28e35bf5ba271bc983e922833b1aad7e9746a
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-11-01 11:56:59 -0700

    Javascript - LinkedList Base

---
+++ javascript/data structures/index.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  //insert first node
+
+  //insert last node
+
+  //insert at index
+
+  //get at index
+
+  //remove at index
+
+  //clear the list
+
+  //print list data
+}
