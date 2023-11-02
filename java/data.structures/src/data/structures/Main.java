79f9e73b6d451cd85eae38dd220304f6293a8678
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-11-02 11:40:37 -0700

    Stack Basics

--- java/data.structures/src/data/structures/Main.java
+++ java/data.structures/src/data/structures/Main.java
@@ -1,16 +1,17 @@
 package data.structures;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
-		var list = new LinkedList(); 
+		//var list = new LinkedList(); 
 		//System.out.println("SIZE of the list is : " + list.size());
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addLast(40);
+//        list.addLast(10);
+//        list.addLast(20);
+//        list.addLast(30);
+//        list.addLast(40);
         //System.out.println("SIZE of the list is : " + list.size());
         //list.reverse();
 //        System.out.println("Index of 20 is : " + list.indexOf(20));
@@ -27,10 +28,25 @@ public class Main {
 //        System.out.println("List contains 30 : " + list.contains(30));
 //		System.out.println("SIZE of the list is : " + list.size());
         
-        var array = list.toArray();
-        System.out.println(Arrays.toString(array));
-        list.printMiddle();
+//        var array = list.toArray();
+//        System.out.println(Arrays.toString(array));
+//        list.printMiddle();
+		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println("Current Stack: " + stack);
+		
+		var poppedValue = stack.pop();
+		System.out.println("Performed Pop(). Popped Value from Stack: " + poppedValue);
+		System.out.println("Current Stack: " + stack);
 
+		var topValue = stack.peek();
+		System.out.println("Top Value from Stack: " + topValue);
+
+		var isEmpty = stack.isEmpty();
+		System.out.println("Is Current Stack Empty? " + isEmpty);
 	}
 
 }
